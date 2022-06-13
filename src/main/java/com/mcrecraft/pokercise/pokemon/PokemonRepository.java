4badc8c044ad48c80bd8213f52701a5b50059f3d
Matt Crecraft <mcrecraft@gmail.com>
2022-06-12 23:36:58 -0500

    Rebuild part 11

---
+++ src/main/java/com/mcrecraft/pokercise/pokemon/PokemonRepository.java
@@ -0,0 +1,8 @@
+package com.mcrecraft.pokercise.pokemon;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PokemonRepository extends CrudRepository<Pokemon, Long> {
+}
