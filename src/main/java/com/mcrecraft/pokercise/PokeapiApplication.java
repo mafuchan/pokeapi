1dba5b75ed187f0d9f2323150599ce7ca93bea5e
Matt Crecraft <mcrecraft@gmail.com>
2022-06-13 01:05:28 -0500

    Rebuild part 15

--- src/main/java/com/mcrecraft/pokercise/PokeapiApplication.java
+++ src/main/java/com/mcrecraft/pokercise/PokeapiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PokeapiApplication {
+public class PokemonApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PokeapiApplication.class, args);
+		SpringApplication.run(PokemonApplication.class, args);
 	}
 
 }
